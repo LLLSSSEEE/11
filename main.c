04419e431ba071fd87981ec1b49b1d278602947b
Chamomile <dltldms5271@gmail.com>
2023-11-16 15:33:02 +0900

    02

--- main.c
+++ main.c
@@ -3,26 +3,17 @@
 
 int main(int argc, char *argv[])
 {
-  int i = 10;
-  char c = 'a';
-  
-  //정수형 포인터 변수 iptr 선언.
-  //iptr이 i를 가리키게 함.
-  int* iptr = &i;
-
-  //문자형 포인터 변수 cptr 선언.
-  //cptr이 c를 가리키게 함.
-  char* cptr = &c; 
-  //정수형 포인터 변수 iptr2 선언.
-  //iptr2가 iptr이 가리키는 곳을 동일하게 가리키게 함.
-  int* iptr2 = iptr; //포인터이기 때문에 변수주소를 이미 갖고 있어서 &안붙여줘도 됨. 
- 
-  
-  
-  
-  printf("i : %p\n%p(size : %i)\n", iptr, &i, sizeof(iptr));
-  printf("c : %p\n%p(size : %i)\n", cptr, &c, sizeof(cptr));
-  printf("iptr2 ; %p, %i\n", iptr2, *iptr2);
+    int i =4; //300으로 변경해서도 해보기.
+    
+    //정수형 포인터 변수 pi 선언. 
+    //pi가 i를 가리키게 함. 
+    int* pi = &i; 
+    
+    //문자형 포인터 변수 pc 선언.
+    //pc가 i를 가리키게 함. 
+    char* pc = &i;
+    
+    printf("%i, %i, %i\n", i, *pi, *pc);
   
   system("PAUSE");	
   return 0;
